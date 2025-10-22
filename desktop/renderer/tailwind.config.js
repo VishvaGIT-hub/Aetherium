484fc1d236d56377c0fa1be5b61b4e33ece00eb8
VishvaGIT-hub <vishvadev543@gmail.com>
2025-10-22 19:02:28 +0530

    Create tailwind.config.js

---
+++ desktop/renderer/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // These will be overridden by inline styles from theme system
+      }
+    },
+  },
+  plugins: [],
+}
